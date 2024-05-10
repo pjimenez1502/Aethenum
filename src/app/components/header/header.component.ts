69d50d5490211097cbb1344759d16c1ff24db0d4
pjimenez1502 <pjimenez1502@gmail.com>
2024-05-10 12:36:16 +0200

    added material-module import

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material-module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MaterialModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
